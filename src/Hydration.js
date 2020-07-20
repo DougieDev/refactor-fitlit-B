69a8c072a602b1a18a5219d7dde713152ba823ef
Joshua Sevy <joshuasevy@outlook.com>
2020-07-20 10:26:30 -0600

    Check each function, comment on each function if used, broken, what it breaks

--- src/Hydration.js
+++ src/Hydration.js
@@ -2,19 +2,28 @@ class Hydration {
   constructor(hydrationData) {
     this.hydrationData = hydrationData;
   }
+
+  /*IF REMOVED, REMOVES ALL DATA EXCEPT USER PERSONAL DATA EX. NAME/ ADDRESS/ ETC...*/
   calculateAverageOunces(id) {
     let perDayUserHydration = this.hydrationData.filter((data) => id === data.userID);
     return perDayUserHydration.reduce((sumSoFar, data) => {
       return sumSoFar += data.numOunces;
     }, 0) / perDayUserHydration.length;
   }
+
+  /*SAME AS ABOVE*/
   calculateDailyOunces(id, date) {
     let findOuncesByDate = this.hydrationData.find((data) => id === data.userID && date === data.date);
     return findOuncesByDate.numOunces;
   }
+
+  /*IF REMOVED, REMOVES ALL DATA EXCEPT USER PERSONAL DATA AND DAILY/ AVERAGE WATER*/
   calculateFirstWeekOunces(userRepo, id) {
     return userRepo.getFirstWeek(id, this.hydrationData).map((data) => `${data.date}: ${data.numOunces}`);
   }
+
+  /*IF REMOVED, REMOVES ALL DATA EXCEPT USER PERSONAL INFO, DAILY WATER/AVERAGE &
+  WATER INTAKE THIS WEEK*/
   calculateRandomWeekOunces(date, id, userRepo) {
     return userRepo.getWeekFromDate(date, id, this.hydrationData).map((data) => `${data.date}: ${data.numOunces}`);
   }
