4fded6493c0d6a6467ba0393d7077669fe882aec
Greyson Elkins <greysonelkins@gmail.com>
2020-07-27 17:27:17 -0600

    Lint User / UserRepo

--- src/UserRepo.js
+++ src/UserRepo.js
@@ -1,5 +1,4 @@
 import Repo from './Repo'
-import User from './User';
 
 
 class UserRepo extends Repo  {
@@ -10,10 +9,15 @@ class UserRepo extends Repo  {
 
   findUserById(id) {
     return this.data.find(dataPoint => dataPoint.id === id)
-    }
+  }
 
   rankUserIDsbyRelevantDataValue(dataSet, date, relevantData, listFromMethod) {
-    let sortedObjectKeys = this.isolateUsernameAndRelevantData(dataSet, date, relevantData, listFromMethod)
+    let sortedObjectKeys = this.isolateUsernameAndRelevantData(
+      dataSet, 
+      date, 
+      relevantData, 
+      listFromMethod
+    )
     return Object.keys(sortedObjectKeys).sort((b, a) => {
       return (sortedObjectKeys[a].reduce((sumSoFar, sleepQualityValue) => {
         sumSoFar += sleepQualityValue
@@ -25,9 +29,24 @@ class UserRepo extends Repo  {
     });
   }
   
-  combineRankedUserIDsAndAveragedData(dataSet, date, relevantData, listFromMethod) {
-    let sortedObjectKeys = this.isolateUsernameAndRelevantData(dataSet, date, relevantData, listFromMethod)
-    let rankedUsersAndAverages = this.rankUserIDsbyRelevantDataValue(dataSet, date, relevantData, listFromMethod)
+  combineRankedUserIDsAndAveragedData(
+    dataSet, 
+    date, 
+    relevantData, 
+    listFromMethod
+  ) {
+    let sortedObjectKeys = this.isolateUsernameAndRelevantData(
+      dataSet, 
+      date, 
+      relevantData, 
+      listFromMethod
+    )
+    let rankedUsersAndAverages = this.rankUserIDsbyRelevantDataValue(
+      dataSet, 
+      date, 
+      relevantData, 
+      listFromMethod
+    )
     return rankedUsersAndAverages.map(rankedUser => {
       rankedUser = {
         [rankedUser]: sortedObjectKeys[rankedUser].reduce(
